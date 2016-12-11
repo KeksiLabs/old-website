5fc075e805c7c211bf86db98e4589f501344a20e
Onni Hakala <onni@keksi.io>
2016-12-11 15:53:28 +0200

    Update travis deploy script

--- _scripts/travis-deploy-to-gh-pages.sh
+++ _scripts/travis-deploy-to-gh-pages.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-ls -lah
 
-env
+# Open the build folder
+cd _site
+
+# Create completely new git repo
+git init .
+
+# Config git
+git config user.name "Travis CI"
+git config user.email "travis@keksi.io"
+
+# Add github as remote
+git remote add github "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+
+# and add all files
+git add -A
+
+# Commit all files
+git commit -am "Builded gh-pages in Travis from master #$TRAVIS_COMMIT"
+
+# Push current master branch as gh-pages into github
+git push github master:gh-pages
+
