29977b6cc7a6414186666654890d46ff4c8f0d0e
Onni Hakala <onni@keksi.io>
2016-12-11 16:40:03 +0200

    Update travis deploy script

--- _scripts/travis-deploy-to-gh-pages.sh
+++ _scripts/travis-deploy-to-gh-pages.sh
@@ -8,16 +8,21 @@ git init .
 
 # Config git
 git config user.name "Keksi Labs Bot"
-git config user.email "Bot@keksi.io"
+git config user.email "bot@keksi.io"
+
+# Add CNAME for the site
+if [ "$SITE_DOMAIN" != "" ]; then
+    echo $SITE_DOMAIN > CNAME
+fi
 
 # Add github as remote
-git remote add github "https://$GITHUB_ACCESS_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git remote add github "https://$GITHUB_ACCESS_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" --quiet
 
 # and add all files
 git add -A
 
 # Commit all files
-git commit -am "Builded gh-pages in Travis from master #$TRAVIS_COMMIT"
+git commit -am "Builded gh-pages from master in Travis\nCommit-ID:$TRAVIS_COMMIT"
 
 # Push current master branch as gh-pages into github
 # Don't output anything so that $GITHUB_ACCESS_TOKEN won't go into the logs
